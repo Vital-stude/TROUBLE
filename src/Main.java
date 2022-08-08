a91c9595c5d0edb85f253fced79272594f5063dd
Vital-stude <110226252+Vital-stude@users.noreply.github.com>
2022-08-08 19:57:34 +0300

    Initial commit

---
+++ src/Main.java
@@ -0,0 +1,8 @@
+public class Main {
+    public static void main(String[] args) {
+        System.out.println("Hello world!");
+     intiOS = 0;
+     int Android = 1;
+     int t = ("iOS && Android") }
+    System.out.println("");
+}
